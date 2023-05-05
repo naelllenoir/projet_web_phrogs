28dfcfcf5ae0cc63fd56f8103a9ac0c042b849f3
naelllenoir <132479150+naelllenoir@users.noreply.github.com>
2023-05-06 01:02:00 +0200

    Update Plotly_1_3_naell_lenoir.py

--- Plotly_1_3_naell_lenoir.py
+++ Plotly_1_3_naell_lenoir.py
@@ -7,7 +7,6 @@ Bubble plot (en grille) avec un point par famille virale et type d’hôte
 ◦ en x : les types d’hôtes
 ◦ en y : la famille virale
 ◦ taille : nombre de virus 
-
 Pour afficher le graphique, il suffit de lancer le script.
 '''
 
@@ -16,17 +15,19 @@ Pour afficher le graphique, il suffit de lancer le script.
 
 from collections import defaultdict
 import re
-import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 
-genomes=pd.read_csv("info_genomes.tsv", sep="\t", index_col="Locus")
+
+### Initialisation ###
 
 virus_host_fam=defaultdict(lambda:defaultdict(int))
 viral_fam=[]
 host_class=[]
 nombre=[]
-dorderhost=defaultdict(list)
+
+
+### Création du double dictionnaire virus_host_fam['famille virale']['hote infecte'] ###
 
 with open("info_genomes.tsv", "r") as infos :
 	for li in infos :
@@ -39,11 +40,10 @@ with open("info_genomes.tsv", "r") as infos :
 		if li[11] != "class" :
 			host=li[11]
 			virus_host_fam[fam][host]+=1
-			if li[12] not in dorderhost[host] :
-				dorderhost[host].append(li[12])
 
-	
-
+			
+### Récupération des données nécessaires ###
+			
 for fam in virus_host_fam :
 	for host in virus_host_fam[fam] :
 		viral_fam.append(fam)
@@ -51,12 +51,18 @@ for fam in virus_host_fam :
 		nombre.append(virus_host_fam[fam][host])
 
 
+### Création du dataframe utile pour créer le graphe ###
 
 df=pd.DataFrame({'Host':list(host_class), 'Virus':list(viral_fam), 'Nb':list(nombre)})
-
 size=df['Nb']
+
+
+### Création de la figure ###
+
 fig=go.Figure(data=[go.Scatter(x=df['Host'], y=df['Virus'], text=df['Nb'], mode='markers', marker=dict(size=size, sizemode='area', sizeref=max(size)/(30.**2), sizemin=2))])
 
+
+### Paramètres esthétiques de la figure ###
 
 fig.update_layout(title=dict(text='Bubble plot du nombre de virus de familles virales infectant leurs hôtes', font=dict(size=20)), title_x=0.5, title_y=0.95,
 	xaxis=dict(
@@ -73,5 +79,7 @@ fig.update_layout(title=dict(text='Bubble plot du nombre de virus de familles vi
 	plot_bgcolor='rgb(243, 243, 243)',
 )
 
-fig.show()
 
+### Affichage de la figure ###
+
+fig.show()
